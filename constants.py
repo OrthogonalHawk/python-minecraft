c17dd08be82f67f02de7d4b5f8e41353c5442978
Orthogonal Hawk <OrthogonalHawk@gmail.com>
2018-04-01 23:14:28 -0500

    Starting a constants file with block definitions

---
+++ constants.py
@@ -0,0 +1,11 @@
+import logging
+
+class McBlockType(object):
+
+    # definitions for the various kinds of MINECRAFT blocks
+    FARMLAND = 60
+    OAK_FENCE = 85
+    
+    def __init__(self):
+        logging.error("This class doesn't do anything...")
+        

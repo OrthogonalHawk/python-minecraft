e6d7b3c12741f765fed0bd3935eaa630df577e53
Orthogonal Hawk <OrthogonalHawk@gmail.com>
2018-04-01 23:15:43 -0500

    Starting to Create a BasicFarm Definition

--- orthogonalhawk_buildings.py
+++ orthogonalhawk_buildings.py
@@ -1,3 +1,5 @@
+import constants
+import copy
 import logging
 import mc_utilities
 
@@ -10,4 +12,28 @@ class SimpleBuilding(mc_utilities.McApiBuilder):
         # define the structure
         for x_idx in range(self.anchor_corner.x, self.anchor_corner.x + 10):
             self.build_steps.append(mc_utilities.McApiSetBlockEvent(x_idx, anchor_corner.y, anchor_corner.z, 103))
-            
+
+class BasicFarm(mc_utilities.McApiBuilder):
+    
+    BORDER_WIDTH = 2
+    
+    def __init__(self, anchor_corner, x_dim, z_dim):
+        mc_utilities.McApiBuilder.__init__(self, "basic_farm", anchor_corner)
+        
+        # define the outer fence corners
+        corner_01, corner_02, corner_03, corner_04 = copy.deepcopy(anchor_corner), copy.deepcopy(anchor_corner), copy.deepcopy(anchor_corner), copy.deepcopy(anchor_corner)
+        corner_02.x += x_dim
+        corner_03.x += x_dim; corner_03.z += z_dim
+        corner_04.z += z_dim
+        
+        logging.info("Defined corners: (%u, %u), (%u, %u), (%u, %u), (%u, %u)" %
+            (corner_01.x, corner_01.z, corner_02.x, corner_02.z,
+             corner_03.x, corner_03.z, corner_04.x, corner_04.z))
+        
+        self._add_blocks_in_line(corner_01, corner_02, constants.McBlockType.OAK_FENCE)
+        self._add_blocks_in_line(corner_02, corner_03, constants.McBlockType.OAK_FENCE)
+        self._add_blocks_in_line(corner_03, corner_04, constants.McBlockType.OAK_FENCE)
+        self._add_blocks_in_line(corner_04, corner_01, constants.McBlockType.OAK_FENCE)
+        
+        
+        

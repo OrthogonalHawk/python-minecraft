e4c42d05cfcf0ecfeb15523b89395348c2771d32
Orthogonal Hawk <OrthogonalHawk@gmail.com>
2018-04-03 22:38:45 -0500

    Adding new Block Type constants

--- constants.py
+++ constants.py
@@ -3,9 +3,13 @@ import logging
 class McBlockType(object):
 
     # definitions for the various kinds of MINECRAFT blocks
+    AIR = 0
     FARMLAND = 60
+    FLOWING_WATER = 8
+    GRASS = 2
     OAK_FENCE = 85
     OAK_FENCE_GATE = 107
+    STILL_WATER = 9
     
     def __init__(self):
         logging.error("This class doesn't do anything...")

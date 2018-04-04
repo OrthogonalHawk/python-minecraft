5b5521d230e2282d9396db5d733d7129e411e756
Orthogonal Hawk <OrthogonalHawk@gmail.com>
2018-04-03 20:55:08 -0500

    Updated Python logging format

--- simple_building.py
+++ simple_building.py
@@ -5,13 +5,15 @@ import mc_utilities
 import orthogonalhawk_buildings
 import time
 
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+
 # create a reference to the game
 mc = Minecraft.create()
 
 base_corner = mc_utilities.Location(455, 63, 44, "base_corner")
 
 # create the structure
-my_building = orthogonalhawk_buildings.SimpleBuilding(base_corner)
+my_building = orthogonalhawk_buildings.BasicFarm(base_corner, 9, 9)
 time.sleep(5)
 my_building.build(mc)
 

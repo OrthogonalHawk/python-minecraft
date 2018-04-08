46f35acac2cb10468bfe409247ab21ea10c1a625
Orthogonal Hawk <OrthogonalHawk@gmail.com>
2018-04-08 15:48:33 -0500

    Adding Block Orientation Information

--- constants.py
+++ constants.py
@@ -1,5 +1,15 @@
 import logging
 
+class McBlockOrientation(object):
+
+    EAST = 0
+    WEST = 1
+    SOUTH = 2
+    NORTH = 3
+
+    def __init__(self):
+        logging.error("This class doesn't do anything...")
+        
 class McBlockType(object):
 
     # definitions for the various kinds of MINECRAFT blocks
@@ -10,8 +20,9 @@ class McBlockType(object):
     OAK_FENCE = 85
     OAK_FENCE_GATE = 107
     STILL_WATER = 9
+    STONE = 1
+    STONE_BRICK_STAIRS = 109
     TORCH = 50
     
     def __init__(self):
         logging.error("This class doesn't do anything...")
-        
